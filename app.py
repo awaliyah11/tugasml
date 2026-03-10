40be1d55622403dc415fade60e1e4301e3a464d2
awaliyah11 <awaliyahfadhilatunnisa@gmail.com>
2026-03-10 15:23:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,15 @@ import numpy as np
 import joblib
 
 # ======================
-# LOAD MODEL, SCALER & ENCODER
+# LOAD MODEL & ENCODER
 # ======================
 model = joblib.load("best_model.pkl")
-scaler = joblib.load("best_scaler.pkl")
 
 le_cut = joblib.load("le_cut.pkl")
 le_color = joblib.load("le_color.pkl")
 le_clarity = joblib.load("le_clarity.pkl")
 
-st.title("Prediksi Harga Diamond - DEBUG READY")
+st.title("Prediksi Harga Diamond")
 st.write("Masukkan karakteristik diamond")
 
 # ======================
@@ -36,21 +35,13 @@ color_encoded = le_color.transform([color])[0]
 clarity_encoded = le_clarity.transform([clarity])[0]
 
 # ======================
-# PREDIKSI & DEBUG
+# PREDIKSI
 # ======================
 if st.button("Prediksi Harga"):
     # Array input sesuai urutan kolom training
     data = np.array([[carat, cut_encoded, color_encoded, clarity_encoded,
                   depth, table, x, y, z]])
     
-    st.write("=== Data Input (sebelum scaling) ===")
-    st.write(data)
+    prediction = model.predict(data)
     
-    # Scaling
-    data_scaled = scaler.transform(data)
-    st.write("=== Data Input (setelah scaling) ===")
-    st.write(data_scaled)
-    
-    # Prediksi
-    prediction = model.predict(data_scaled)
     st.success(f"Perkiraan Harga Diamond: ${prediction[0]:,.2f}")
